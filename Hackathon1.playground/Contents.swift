850f6ee5ceaf8bbe7be1ef7654576c2d66cdc9d5
Andy Kim <andy.kim@optus.com.au>
2018-07-29 15:20:18 +1000

    refactor

--- Hackathon1.playground/Contents.swift
+++ Hackathon1.playground/Contents.swift
@@ -2,31 +2,6 @@
 
 import UIKit
 
-func median(ranges: [CountableClosedRange<Int>]) -> Int {
-    var max = 0
-    var min = 0
-    ranges.forEach {
-        max += $0.upperBound
-        min += $0.lowerBound
-    }
-    return Int(ceil(Float(max+min)/Float(ranges.count)))
-}
-
-func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
-    if range.lowerBound >= range.upperBound {
-        return nil
-    } else {
-        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-        if a[midIndex] > key {
-            return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
-        } else if a[midIndex] < key {
-            return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
-        } else {
-            return midIndex
-        }
-    }
-}
-
 func closest(value: Int, from range: CountableClosedRange<Int>) -> Int {
     let pick: Int
     if value < range.lowerBound {
@@ -39,9 +14,7 @@ func closest(value: Int, from range: CountableClosedRange<Int>) -> Int {
     return pick
 }
 
-func minimumSum(ranges: [CountableClosedRange<Int>]) -> Int {
-    let medianValue = median(ranges: ranges)
-    print("medianValue: \(medianValue)")
+func getsum(value: Int, ranges: [CountableClosedRange<Int>]) -> Int {
     var sum = 0
     var prevValue: Int?
     for (index, range) in ranges.enumerated() {
@@ -49,9 +22,8 @@ func minimumSum(ranges: [CountableClosedRange<Int>]) -> Int {
         if let prevValue = prevValue {
             pick = closest(value: prevValue, from: range)
             sum += abs(prevValue - pick)
-            print("prevValue: \(prevValue) pick: \(pick)")
         } else {
-            pick = closest(value: medianValue, from: range)
+            pick = closest(value: value, from: range)
             if ranges.count > index + 1 {
                 let nextRange = ranges[index+1]
                 if !nextRange.contains(pick) {
@@ -59,12 +31,40 @@ func minimumSum(ranges: [CountableClosedRange<Int>]) -> Int {
                     pick = closest(value: nextClosest, from: range)
                 }
             }
-            print("first pick: \(pick)")
         }
-        print("sum: \(sum)")
         prevValue = pick
     }
     return sum
+}
+
+func minimumSum(ranges: [CountableClosedRange<Int>]) -> Int {
+    let range = ranges[0]
+    var minimumSum = Int.max
+    for (_, value) in range.enumerated() {
+        var sum = 0
+        var prevValue: Int?
+        for (index, range) in ranges.enumerated() {
+            var pick: Int
+            if let prevValue = prevValue {
+                pick = closest(value: prevValue, from: range)
+                sum += abs(prevValue - pick)
+            } else {
+                pick = closest(value: value, from: range)
+                if ranges.count > index + 1 {
+                    let nextRange = ranges[index+1]
+                    if !nextRange.contains(pick) {
+                        let nextClosest = closest(value: pick, from: nextRange)
+                        pick = closest(value: nextClosest, from: range)
+                    }
+                }
+            }
+            prevValue = pick
+        }
+        if minimumSum > sum {
+            minimumSum = sum
+        }
+    }
+    return minimumSum
 }
 
 let rangesArray: [[CountableClosedRange<Int>]] = [[1...3, 3...7, (-2)...0],
@@ -84,7 +84,7 @@ let rangesArray: [[CountableClosedRange<Int>]] = [[1...3, 3...7, (-2)...0],
 
 rangesArray.forEach {
     print("============")
-    print("ranges: (\($0.map { "\($0.lowerBound...$0.upperBound)" })")
+    print("ranges: (\($0.map { "\($0.lowerBound...$0.upperBound)" }.joined(separator: ", " )))")
     let number = minimumSum(ranges: $0)
     print("minimumSum: (\(number))")
     print("============")
